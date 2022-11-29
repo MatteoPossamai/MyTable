0299f1662cc53ca830ebf0aa0ec4cbc2bf60d3ac
poss03251 <mpossamaim@gmail.com>
2022-11-29 18:01:30 +0100

    added all the url file to the main one, so all the requests can be handled

--- api/digital_menu_api/urls.py
+++ api/digital_menu_api/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+base_url = 'api/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(base_url, include('accounts.urls')),
+    path(base_url, include('order.urls')),
+    path(base_url, include('restaurant.urls')),
 ]
