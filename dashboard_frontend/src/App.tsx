2605826f7628e7d975d1a2f7f50ec0a66f7289d9
poss03251 <mpossamaim@gmail.com>
2022-12-01 21:17:14 +0100

    started base of app

--- dashboard_frontend/src/App.tsx
+++ dashboard_frontend/src/App.tsx
@@ -1,10 +1,20 @@
+// Global imports
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Local imports
+// Components
+import MainPage from './components/main';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="login" element={<h1>Login</h1>} />
+        <Route path="dashboard/:id" element={<MainPage />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ dashboard_frontend/src/components/main.tsx
@@ -0,0 +1,15 @@
+// Local imports
+// Components
+import Header from "./header";
+import LateralNav from "./lateral_nav";
+
+function MainPage(){
+    return (
+        <main>
+            <Header />
+            <LateralNav />
+        </main>
+    )
+}
+
+export default MainPage;
