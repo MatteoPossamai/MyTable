9b9bae2b0b6de42137a670baa757571d1a4b7f8c
poss03251 <mpossamaim@gmail.com>
2022-12-17 21:43:19 +0100

    file moving, removinf of the footer for now

--- mobile_frontend/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return (
-        <footer className="footer">
-            <p>Powered by <a href="https://mytablelanding.netlify.app/">MyTable</a> </p>
-        </footer>
-    );
-}
-
-export default Footer;
