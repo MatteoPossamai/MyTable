9a9dbcaf426ca6509025146e323db0876d922ce3
poss03251 <mpossamaim@gmail.com>
2022-12-19 18:03:55 +0100

    fixed loader and added new style

--- mobile_frontend/src/components/loader.tsx
+++ mobile_frontend/src/components/loader.tsx
@@ -1,8 +1,8 @@
 function Loader() {
   return (
     <div className="loader">
-      <svg>
-        <circle cx="70" cy="70" />
+      <svg className="svg">
+        <circle cx="70" cy="70" r="60" height="100px" width="100px" />
       </svg>
     </div>
   );
