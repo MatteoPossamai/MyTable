487f46bd5f21a75b1a1682c12dae423ed8bceea1
poss03251 <mpossamaim@gmail.com>
2023-01-04 22:17:26 +0100

    initialized the settings section

--- dashboard_frontend/src/components/settings.tsx
+++ dashboard_frontend/src/components/settings.tsx
@@ -6,7 +6,21 @@ function Settings(){
     return (
         <>
             <h1 className="topHeading">Settings</h1>
-        </>
+            <div className="settingsContainer">
+                <section className="settingsSection accountSection">
+                    <h2>Account</h2>
+                </section>
+
+                <section className="settingsSection planSection">
+                    <h2>Plan</h2>
+                </section>
+
+                <section className="settingsSection contactSection">
+                    <h2>Contact</h2>
+                </section>
+
+            </div>
+        </> 
     )
 }
 
