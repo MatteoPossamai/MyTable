3a0d503e825f1823a0101897370ef0dbeab6e500
poss03251 <mpossamaim@gmail.com>
2023-01-05 10:25:58 +0100

    Added notes

--- dashboard_frontend/src/components/dashboard.tsx
+++ dashboard_frontend/src/components/dashboard.tsx
@@ -1,6 +1,11 @@
 function DashboardCard(){
     return (
         <>
+            { /* Statistiche */}
+            { /* QR code */}
+            { /* Prenotazioni (futuro) */}
+            { /* Ordini (futuro) */}
+            
             <h1 className="topHeading">Dashboard</h1>
         </>
     )
