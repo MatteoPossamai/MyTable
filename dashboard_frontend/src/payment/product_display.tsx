eda803033a12416c4e074c506ac9d17c0491fefd
poss03251 <mpossamaim@gmail.com>
2023-01-23 17:50:23 +0100

    Added tests for multiple items

--- dashboard_frontend/src/payment/product_display.tsx
+++ dashboard_frontend/src/payment/product_display.tsx
@@ -1,6 +1,7 @@
 import Logo from "./logo";
 
 const ProductDisplay = () => (
+    
     <section>
       <div className="product">
         <Logo />
@@ -9,9 +10,10 @@ const ProductDisplay = () => (
           <h5>$20.00 / month</h5>
         </div>
       </div>
-      <form action="/create-checkout-session" method="POST">
+      <form action="http://127.0.0.1:8000/api/v1/stripe/create-checkout-session/" method="POST">
         {/* Add a hidden field with the lookup_key of your Price */}
-        <input type="hidden" name="lookup_key" value="{{PRICE_LOOKUP_KEY}}" />
+        <input type="hidden" name="lookup_key" value="1" />
+        <input type="hidden" name="lookup_key" value="122" />
         <button id="checkout-and-portal-button" type="submit">
           Checkout
         </button>
--- dashboard_frontend/src/payment/success_display.tsx
+++ dashboard_frontend/src/payment/success_display.tsx
@@ -1,28 +1,28 @@
 import Logo from "./logo";
 
 
-const SuccessDisplay = (props:{ sessionId: any }) => {
-    return (
-      <section>
-        <div className="product Box-root">
-          <Logo />
-          <div className="description Box-root">
-            <h3>Subscription to starter plan successful!</h3>
-          </div>
+const SuccessDisplay = (props: { sessionId: any }) => {
+  return (
+    <section>
+      <div className="product Box-root">
+        <Logo />
+        <div className="description Box-root">
+          <h3>Subscription to starter plan successful!</h3>
         </div>
-        <form action="/create-portal-session" method="POST">
-          <input
-            type="hidden"
-            id="session-id"
-            name="session_id"
-            value={props.sessionId}
-          />
-          <button id="checkout-and-portal-button" type="submit">
-            Manage your billing information
-          </button>
-        </form>
-      </section>
-    );
-  };
+      </div>
+      <form action="http://127.0.0.1:8000/api/v1/stripe/create-checkout-session/" method="POST">
+        <input
+          type="hidden"
+          id="session-id"
+          name="session_id"
+          value={props.sessionId}
+        />
+        <button id="checkout-and-portal-button" type="submit">
+          Manage your billing information
+        </button>
+      </form>
+    </section>
+  );
+};
 
 export default SuccessDisplay;
