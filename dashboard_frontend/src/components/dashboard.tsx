fb8149024c84504affa006cb80c0ef10c61e74d0
poss03251 <mpossamaim@gmail.com>
2023-01-27 20:57:36 +0100

    added the qr code and started the contact page

--- dashboard_frontend/src/components/dashboard.tsx
+++ dashboard_frontend/src/components/dashboard.tsx
@@ -1,12 +1,33 @@
+import {QRCodeSVG} from 'qrcode.react';
+import "../styles/dashboard.css";
+
 function DashboardCard(){
     return (
         <>
             { /* Statistiche */}
-            { /* QR code */}
             { /* Prenotazioni (futuro) */}
             { /* Ordini (futuro) */}
             
             <h1 className="topHeading">Dashboard</h1>
+            <div className="dashboardWrapper">
+                <section className="qrSection">
+                    <h2>QR Code</h2>
+                    <QRCodeSVG 
+                        value="https://reactjs.org/" 
+                        size={256}
+                        level='H'
+                        includeMargin={true}
+                        imageSettings={
+                            {
+                                src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
+                                height: 50,
+                                width: 50,
+                                excavate: true
+                            }
+                        }
+                        />
+                </section>
+            </div>
         </>
     )
 }
--- dashboard_frontend/src/components/settings.tsx
+++ dashboard_frontend/src/components/settings.tsx
@@ -17,6 +17,22 @@ function Settings(){
 
                 <section className="settingsSection contactSection">
                     <h2>Contact</h2>
+                    <p>If you need any assistance, don't exitate to contact us. </p>
+                    <p>Here are our contacts</p>
+                    <ul>
+                        <li>Phone: +39 123 456 789</li>
+                        <li>Email:
+                            <a href="mailto: prova@gmail.com"> prova@gmail.com </a>
+                        </li>
+                    </ul>
+                    <p>Otherwise, fill the form to open a ticket. We will reply back as soon as possible</p>
+                    <form>
+                        <label htmlFor="subject">Subject</label>
+                        <input type="text" name="subject" id="subject" placeholder="Subject" />
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" placeholder="Message" />
+                        <button type="submit">Send</button>
+                    </form>
                 </section>
 
             </div>
