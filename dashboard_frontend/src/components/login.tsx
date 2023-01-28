1adf52047a56271db19317290e66e6c1abd61639
poss03251 <mpossamaim@gmail.com>
2023-01-28 18:12:51 +0100

    modified login path

--- dashboard_frontend/src/components/login.tsx
+++ dashboard_frontend/src/components/login.tsx
@@ -1,5 +1,20 @@
-// Complete login/signup component
+// Global imports
+import { useState } from "react";
+
+// Local imports
+// Styles
+import '../styles/login.css';
+
 function LoginPage(){
+    // Handle the login form
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Handle the signup form
+    const [signupEmail, setSignupEmail] = useState("");
+    const [signupPassword, setSignupPassword] = useState("");
+    const [signupConfirmPassword, setSignupConfirmPassword] = useState("");
+
     return (
         <div className="authContainer">
             <section className="loginSection">
@@ -7,17 +22,11 @@ function LoginPage(){
                 <hr />
                 <form className="authForm">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     <button type="submit">Login</button>
                 </form>
-            </section>
-
-            <section className="googleSection">
-                <h1>Google</h1>
-                <hr />
-
             </section>
             
             <section className="signupSection">
@@ -25,11 +34,11 @@ function LoginPage(){
                 <hr />
                 <form className="authForm">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input type="email" name="email" id="email" value={signupEmail} onChange={(e) => setSignupEmail(e.target.value)} />
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input type="password" name="password" id="password" value={signupPassword} onChange={(e) => setSignupPassword(e.target.value)} />
                     <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password" name="confirmPassword" id="confirmPassword" />
+                    <input type="password" name="ccp" id="cp" value={signupConfirmPassword} onChange={(e) => setSignupConfirmPassword(e.target.value)} />
                     <button type="submit">Signup</button>
                 </form>
             </section>
